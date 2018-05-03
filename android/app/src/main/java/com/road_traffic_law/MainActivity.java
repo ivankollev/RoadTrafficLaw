3acf1ff96460c9dede2e00cf8c11c5169f949764
ivan.kolev <ivan.kolev@accedia.com>
2018-05-03 13:54:01 +0300

    initial commit

---
+++ android/app/src/main/java/com/road_traffic_law/MainActivity.java
@@ -0,0 +1,15 @@
+package com.road_traffic_law;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "road_traffic_law";
+    }
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('road_traffic_law', () => App);
